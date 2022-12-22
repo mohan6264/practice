aac12eb3ee5a326f80c16b1ed0d10e4b58202770
master <master@DESKTOP-TJH37OU>
2022-12-22 17:14:44 +0530

    commit

---
+++ src/keywords/DataTypes.java
@@ -0,0 +1,25 @@
+package keywords;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		boolean one=true;
+		byte a=1;
+		short b=2;
+		int i=21;
+		long c=21L;
+		float d=0.34f;
+		double e=21.23;
+		char f='a';
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(i);
+		System.out.println(one);
+	
+	}
+
+}
